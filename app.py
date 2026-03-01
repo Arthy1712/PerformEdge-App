1d038ed177c9afdd6e8caa77409f1f31c8e3a66f
Arthy Murugan <arthy.cse2022@jerusalemengg.ac.in>
2026-03-01 22:30:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import time
 # CONFIG
 # ----------------------------
 DB_PATH = "users.db"
-
 HR_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge_Dashboard/Dashboard1"
 MANAGER_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge_Dashboard/Dashboard2"
 EMPLOYEE_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge_Dashboard/Dashboard3"
