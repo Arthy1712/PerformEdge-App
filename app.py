404c9789263039fee7f96fe1bfaa5973da6ee6e4
Arthy Murugan <arthy.cse2022@jerusalemengg.ac.in>
2026-03-21 21:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import time
 DB_PATH = "users.db"
 HR_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge-DB/Dashboard1"
 MANAGER_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge-DB/Dashboard2"
-EMPLOYEE_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge-DB/Dashboard3"
+EMPLOYEE_TABLEAU_URL = "https://public.tableau.com/views/PerformEdge-DBtill21/Dashboard3"
 # ----------------------------
 # PASSWORD HASH FUNCTION
 # ----------------------------
